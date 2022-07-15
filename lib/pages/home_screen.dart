14a889d340f09e47024d05eb1fc6d1d0b9b61398
mahdiAshkar <mahdi81.it@gmail.com>
2022-07-15 19:57:11 +0430

    implement top bar and bottom

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -11,30 +11,78 @@ class HomeScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: klightBluecolor,
+        elevation: 0,
         child: const Icon(Icons.add),
       ),
       body: SafeArea(
-          child: Stack(children: [
-        SizedBox(
+          child: Stack(alignment: Alignment.bottomCenter, children: [
+        Container(
           width: Get.width,
+          color: klightBluecolor,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: kbuttontopcolor,
-                    )),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.menu,
-                      color: kbuttontopcolor,
-                    )),
-              ]),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_back_ios,
+                            color: kbuttontopcolor,
+                          )),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.menu,
+                            color: kbuttontopcolor,
+                          )),
+                    ]),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 40, top: 20),
+                child: CircleAvatar(
+                  backgroundColor: kcircularAvatarcolor,
+                  radius: 25,
+                  child: Center(child: Icon(Icons.bookmarks)),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 45),
+                child: Text(
+                  'All',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: fontcolor),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 45),
+                child: Text(
+                  'Tasks ',
+                  style: TextStyle(fontSize: 15, color: fontcolor),
+                ),
+              ),
             ],
           ),
+        ),
+        Container(
+          width: Get.width,
+          height: Get.height * 0.65,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                  topRight: Radius.circular(25.0))),
         )
       ])),
     );
