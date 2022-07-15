6a6ab38161ade1e0f67d3b99d6875593e4314968
mahdiAshkar <mahdi81.it@gmail.com>
2022-07-15 16:49:34 +0430

    create routes.dart and change GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.fadeIn,
+      getPages: Routes.pages,
+    );
   }
 }
