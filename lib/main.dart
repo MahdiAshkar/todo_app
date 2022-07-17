d29770995c26ac1d95e6eb30d969f0443083047b
mahdiAshkar <mahdi81.it@gmail.com>
2022-07-17 20:30:52 +0430

    finished project and add icon ios & android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todo/bindings/mybindings.dart';
 import 'package:todo/routes/routes.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -4,4 +4,13 @@ class TaskModel {
   bool? status;
   TaskModel(
       {required this.title, required this.subtitle, required this.status});
+
+  Map<String, dynamic> toJson() {
+    return {'title': title, 'subtitle': subtitle, 'status': status};
+  }
+
+  TaskModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        subtitle = json['subtitle'],
+        status = json['status'];
 }
